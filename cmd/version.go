862183c6892fb4fc27df492d787eb793e6550a1e
Shane P. Lawrence <me@shanelawrence.info>
2018-10-03 12:50:19 -0400

    Use goreleaser/makefile to set version.

--- cmd/version.go
+++ cmd/version.go
@@ -1,31 +1,38 @@
 package cmd
 
 import (
+	"os"
+
 	"github.com/hashicorp/go-version"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"os"
+)
+
+// Placeholder values will be overridden by goreleaser or makefile.
+var (
+	Version   = "0.0.0"
+	Commit    = "ffffffff"
+	BuildDate = "2006-01-02T15:04:05Z07:00"
 )
 
 func init() {
 	RootCmd.AddCommand(versionCmd)
 }
 
-// Version is the semantic versioning number for kubeaudit.
-const Version = "0.1.0"
-
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of kubeaudit",
 	Long:  `This just prints the version of kubeaudit`,
 	Run: func(cmd *cobra.Command, args []string) {
-		ver, err := version.NewVersion(Version)
+		_, err := version.NewVersion(Version + "+" + Commit)
 		if err != nil {
 			log.Error(err)
 			os.Exit(1)
 		}
 		log.WithFields(log.Fields{
-			"Version": ver,
+			"BuildDate": BuildDate,
+			"Commit":    Commit,
+			"Version":   Version,
 		}).Info("Kubeaudit")
 
 		kubeconfig := rootConfig.kubeConfig
